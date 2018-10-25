f84657da40e1ad5df0e1adc1dada45963933510c
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-10-25 10:01:09 -0500

    Suma productos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './components/Login' 
+import firebaseApp from './config/firebase' 
+import Nav from './components/Navbar';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null,
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener() {
+    firebaseApp.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({ user });
+      } else{
+        this.setState({ user: null });
+      }
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+      <div>
+
+      {this.state.user ? (< Nav user={this.state.user} />) : (<Login />)}
+     
+        
       </div>
     );
   }
